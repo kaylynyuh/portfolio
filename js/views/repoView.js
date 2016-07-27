871af1aca2f4760992eed2fca33a2b4ae3bcef97
kaylynyuh <yuhkay4@gmail.com>
2016-07-27 12:17:27 -0700

    more changes...

--- js/views/repoView.js
+++ js/views/repoView.js
@@ -1,23 +1,16 @@
 (function(module) {
   var repoView = {};
 
-  /* Let's compile our new template!
-       Save the result of invoking Handlebars in this 'repoCompiler' variable
-       that we will pass to the append method below. */
-
   var repoCompiler = Handlebars.compile($('#repo-template').text());
 
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
-      reposObj.withTheAttribute('forks')  //experiment changing this attribute!
+      reposObj.withTheAttribute('name')  
       .map(repoCompiler)
     );
-    // console.log(reposObj.withTheAttribute('forks'));
+    console.log(reposObj.withTheAttribute('name'));
   };
-/*  Call the function that loads (or 'requests') our repo data.
-    Pass in some view function as a higher order callback, so our repos
-    will render after the data is loaded. */
 
   reposObj.requestRepos(repoView.renderRepos);
   module.repoView = repoView;
